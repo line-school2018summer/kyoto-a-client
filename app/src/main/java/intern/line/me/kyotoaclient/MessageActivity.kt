8facbd1d122ab525a2b79ab49ee0a0669e568916
pinfort <ptg@nijitei.com>
2018-08-16 22:43:12 +0900

    message activityを追加

---
+++ app/src/main/java/intern/line/me/kyotoaclient/MessageActivity.kt
@@ -0,0 +1,12 @@
+package intern.line.me.kyotoaclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message)
+    }
+}
