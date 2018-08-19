328eb0510b338752fc30f350534e25ae437f031b
Naoki Kishi <naokikishi.kyoto@gmail.com>
2018-08-19 21:19:38 +0900

    FirebaseUIを用いたAuthActivityを実装 #5

---
+++ app/src/main/java/intern/line/me/kyotoaclient/AuthActivity.kt
@@ -0,0 +1,75 @@
+package intern.line.me.kyotoaclient
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
+
+class AuthActivity : AppCompatActivity() {
+
+    companion object {
+        private const  val RC_SIGN_IN = 123
+        private val auth = FirebaseAuth.getInstance()!!
+
+        fun intent(context: Context): Intent =
+                Intent(context,AuthActivity::class.java)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        if(auth.currentUser != null){
+            LoginedActivity.intent(this).let { startActivity(it) }
+        }
+
+        val providers = Arrays.asList(AuthUI.IdpConfig.GoogleBuilder().build(),
+                AuthUI.IdpConfig.EmailBuilder().build())
+
+        //FirebaseUIのログインページに飛ぶ
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(), RC_SIGN_IN)
+
+    }
+
+
+    //戻るボタンで戻ってきたときの処理
+    override fun onResume(){
+        super.onResume()
+
+        //ログインされてたらもとのページに戻る
+        if(auth.currentUser != null){
+            finish()
+        }
+
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            val response = IdpResponse.fromResultIntent(data)
+            if(resultCode == Activity.RESULT_OK){
+                LoginedActivity.intent(this).let { startActivity(it) }
+                return
+            }
+            else {
+                //ログアウトした後にログインが必要な画面に戻ると発生する
+                if(response == null){
+                    AuthActivity.intent(this).let{startActivity(it)}
+                    return
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/intern/line/me/kyotoaclient/LoginedActivity.kt
@@ -0,0 +1,40 @@
+package intern.line.me.kyotoaclient
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_logined.*
+
+class LoginedActivity : AppCompatActivity() {
+    companion object {
+
+        fun intent(context: Context): Intent =
+                Intent(context,LoginedActivity::class.java)
+    }
+
+    lateinit var mAuth : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logined)
+
+        mAuth = FirebaseAuth.getInstance()
+
+
+        val user : FirebaseUser? = mAuth.getCurrentUser()
+
+        if (user != null){
+            uid.text = "uid : " + user.uid
+            name_text_view.text = "name : " + user.displayName
+        }
+
+
+        logout_button.setOnClickListener{
+            mAuth.signOut()
+            AuthActivity.intent(this).let{startActivity(it)}
+            }
+    }
+}
