dc3c896429a9d659dcda829bd62b60751e00a541
Naoki Kishi <naokikishi.kyoto@gmail.com>
2018-08-23 23:16:08 +0900

    トークン認証のライブラリを追加。

--- app/src/main/java/intern/line/me/kyotoaclient/AuthActivity.kt
+++ app/src/main/java/intern/line/me/kyotoaclient/AuthActivity.kt
@@ -5,9 +5,11 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import intern.line.me.kyotoaclient.lib.api.CreateUserPresenter
 import java.util.*
 
 class AuthActivity : AppCompatActivity() {
@@ -27,17 +29,18 @@ class AuthActivity : AppCompatActivity() {
 
         if(auth.currentUser != null){
             RoomListActivity.intent(this).let { startActivity(it) }
+        }else {
+
+            val providers = Arrays.asList(AuthUI.IdpConfig.GoogleBuilder().build(),
+                    AuthUI.IdpConfig.EmailBuilder().build())
+
+            //FirebaseUIのログインページに飛ぶ
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(), RC_SIGN_IN)
         }
-
-        val providers = Arrays.asList(AuthUI.IdpConfig.GoogleBuilder().build(),
-                AuthUI.IdpConfig.EmailBuilder().build())
-
-        //FirebaseUIのログインページに飛ぶ
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(), RC_SIGN_IN)
 
     }
 
@@ -50,17 +53,30 @@ class AuthActivity : AppCompatActivity() {
         if(auth.currentUser != null){
             finish()
         }
-
     }
+
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        Log.d("AuthActivity","start onActivityResult")
         if(requestCode == RC_SIGN_IN){
+
             val response = IdpResponse.fromResultIntent(data)
-            if(resultCode == Activity.RESULT_OK){
-                LoginedActivity.intent(this).let { startActivity(it) }
+
+            if(resultCode == Activity.RESULT_OK) {
+
+
+                Log.d("AuthActivity", "start pcreating user")
+
+                //トークンが必要なAPIを叩く場合はstartWithGettingToken(user)でラップする
+                val user = auth.currentUser
+                if(user != null) {
+                    CreateUserPresenter(user.displayName!!).start()
+                }
+
+                RoomListActivity.intent(this).let { startActivity(it) }
                 return
             }
             else {
---
+++ app/src/main/java/intern/line/me/kyotoaclient/lib/api/UserPresenter.kt
@@ -0,0 +1,49 @@
+package intern.line.me.kyotoaclient.lib.api
+
+import com.google.firebase.auth.FirebaseAuth
+import intern.line.me.kyotoaclient.AuthActivity
+import intern.line.me.kyotoaclient.lib.User
+import intern.line.me.kyotoaclient.lib.api.interfaces.UserAPI
+import intern.line.me.kyotoaclient.lib.firebase.FirebaseUtil
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.withContext
+import retrofit2.HttpException
+
+//一つのメソッドに対して一つのクラスを作成。引数はクラスのコンストラクタを利用
+class CreateUserPresenter(val name: String): API(){
+    private val api = retrofit.create(UserAPI::class.java)
+
+    private suspend fun createASyncUser(name: String,token : String) : User = withContext(CommonPool) {
+            api.createUser(name, token).await()
+    }
+
+    private suspend fun createUser() {
+
+        //トークンは以下のメソッドで取得できるが、FirebaseUtil.startWithGettingToken{}でラップしていないとnullが返ってくる
+        val token = FirebaseUtil().getIdToken()
+
+        try {
+            if (token != null) {
+                createASyncUser(name, token)
+            } else {
+                throw Exception("can't get token.")
+            }
+
+        } catch (t: HttpException) {
+            throw Exception("can't access server.")
+        }
+    }
+
+    override fun start() {
+        val auth = FirebaseAuth.getInstance()!!
+        val user = auth.currentUser
+        if(user != null) {
+            FirebaseUtil().startWithGettingToken(user) {
+                launch(this.job + UI) { createUser() }
+            }
+        }
+
+    }
+}
