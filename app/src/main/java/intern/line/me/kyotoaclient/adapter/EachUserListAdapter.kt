3dd8a34ec6166c671bde6d00dd5eb57f315b1b8e
face9363 <4181hiro@gmail.com>
2018-08-26 00:09:54 +0900

    API継承クラス

---
+++ app/src/main/java/intern/line/me/kyotoaclient/adapter/EachUserListAdapter.kt
@@ -0,0 +1,44 @@
+package intern.line.me.kyotoaclient
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewParent
+import android.widget.BaseAdapter
+import android.widget.TextView
+import java.text.FieldPosition
+
+class UserListAdapter(private val context: Context,
+                      private val nonUidUserList: Array<NonUidUser>): BaseAdapter() {
+    private val inflater = LayoutInflater.from(context)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = convertView ?: inflater.inflate(R.layout.user_list_row, parent, false)
+        val content = ViewHolder(view)
+        val name = nonUidUserList[position].name
+        content.name.text = name
+        content.id.text = "id: ${getItemId(position).toString()}"
+        content.icon.text = name.substring(0,1)
+        return view
+    }
+
+    override fun getItem(position: Int): NonUidUser {
+        return nonUidUserList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return nonUidUserList[position].id
+    }
+
+    override fun getCount(): Int {
+        return nonUidUserList.count()
+    }
+
+}
+
+private class ViewHolder(view: View){
+    val name = view.findViewById<TextView>(R.id.name_text)
+    val id = view.findViewById<TextView>(R.id.id_text)
+    val icon = view.findViewById<TextView>(R.id.name_icon)
+}
