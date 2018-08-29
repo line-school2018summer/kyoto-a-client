fd718848050925b68f7dc621f690507e96cb6cde
nullcline <hi.suguru@gmail.com>
2018-08-29 20:06:28 +0900

    ユーザー仮アイコン設置

--- app/src/main/java/intern/line/me/kyotoaclient/adapter/UserListAdapter.kt
+++ app/src/main/java/intern/line/me/kyotoaclient/adapter/UserListAdapter.kt
@@ -17,6 +17,25 @@ class UserListAdapter(private val context: Context, private val realm_results : 
         this.layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
     }
 
+<<<<<<< HEAD
+=======
+
+    override fun getCount(): Int {
+        return users.size
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        var convertView = layoutInflater.inflate(R.layout.user_list_row, parent, false)
+        (convertView.findViewById(R.id.name_text) as TextView).setText(users[position].name)
+        (convertView.findViewById(R.id.name_icon) as TextView).setText(users[position].name.substring(0,1))
+        return convertView
+    }
+
+    override fun getItem(position: Int): User {
+        return users[position]
+    }
+
+>>>>>>> ユーザー仮アイコン設置
     override fun getItemId(position: Int): Long {
         return super.getItem(position)?.id ?: 0
     }
