d37f58381faaa70087fa20e61516af620742a9a7
nullcline <hi.suguru@gmail.com>
2018-09-13 16:37:16 +0900

    ルームメンバー取得API

---
+++ app/src/main/java/intern/line/me/kyotoaclient/presenter/room/GetMembers.kt
@@ -0,0 +1,39 @@
+package intern.line.me.kyotoaclient.presenter.user
+
+import intern.line.me.kyotoaclient.lib.api.interfaces.RoomsAPI
+import intern.line.me.kyotoaclient.lib.firebase.FirebaseUtil
+import intern.line.me.kyotoaclient.model.entity.User
+import intern.line.me.kyotoaclient.model.repository.UserRepository
+import intern.line.me.kyotoaclient.presenter.API
+import io.realm.RealmResults
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.withContext
+import retrofit2.HttpException
+import ru.gildor.coroutines.retrofit.await
+
+class GetMembers: API() {
+    val api = retrofit.create(RoomsAPI::class.java)
+    private val repo = UserRepository()
+
+    private suspend fun getAsyncMembers(token: String, room_id: Long): List<User> = withContext(CommonPool) {
+        api.getMembers(token, room_id).await()
+    }
+
+    suspend fun getMembers(room_id: Long): List<User> {
+        try {
+            val token = FirebaseUtil().getToken() ?: throw Exception("can't get token.")
+
+            val members =  getAsyncMembers(token, room_id)
+
+//            repo.updateAll(members)
+            return members
+
+        } catch (t: HttpException) {
+            throw Exception("update failed.")
+        }
+    }
+
+//    fun getMembersFromDb(room_id: Long) : RealmResults<User>{
+//        return repo.getAll(room_id)
+//    }
+}
