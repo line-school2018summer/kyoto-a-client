3b1d17db5435681375e72795e259b0791d490dde
pinfort <ptg@nijitei.com>
2018-09-21 00:59:53 +0900

    ユーザーの方も

--- app/src/main/java/intern/line/me/kyotoaclient/activity/ChangeMyProfileActivity.kt
+++ app/src/main/java/intern/line/me/kyotoaclient/activity/ChangeMyProfileActivity.kt
@@ -1,6 +1,8 @@
 package intern.line.me.kyotoaclient.activity
 
 import android.Manifest
+import android.content.ContentUris
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
@@ -8,14 +10,17 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.view.View
+import android.webkit.MimeTypeMap
 import android.widget.ImageView
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.google.android.gms.common.util.IOUtils
 import intern.line.me.kyotoaclient.R
 import intern.line.me.kyotoaclient.model.entity.User
 import intern.line.me.kyotoaclient.presenter.user.*
@@ -25,6 +30,7 @@ import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
 import java.io.File
+import java.io.FileOutputStream
 import java.io.UnsupportedEncodingException
 import java.net.URLDecoder
 
@@ -106,40 +112,93 @@ class ChangeMyProfileActivity : AppCompatActivity() {
             if (requestCode == CHOSE_FILE_CODE && resultCode == RESULT_OK && data != null) {
                 val uri = Uri.parse(data.dataString)
 
-                val column = arrayOf(MediaStore.Images.Media.DATA)
-                val cursor: Cursor?
-                val checkUri: String = uri.toString().replace("content://", "")
-                if (checkUri.indexOf(':') != -1 || checkUri.indexOf("%3A") != -1) {
-                    val fileId = DocumentsContract.getDocumentId(uri)
-                    println(uri)
-                    val id = fileId.split(":")[1]
-                    val selector = MediaStore.Images.Media._ID + "=?"
-                    cursor = context.contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, column, selector, arrayOf(id), null)
+                val f = getFile(uri)
+                if (f == null) {
+                    Toast.makeText(this, "適切でないファイル形式です", Toast.LENGTH_SHORT).show()
                 } else {
-                    cursor = context.contentResolver.query(uri, column, null, null, null)
-                }
-                var path: String? = null
-                val columnIndex = cursor.getColumnIndex(column[0])
-                if (cursor != null) {
-                    if (cursor.moveToFirst()) {
-                        path = cursor.getString(columnIndex)
-                    }
-                    cursor.close()
-                    if (path != null) {
-                        file = File(path)
-                    }
-
+                    file = f
                     launch(job + UI) {
                         PostIcon(file).postIcon()
                         setImgByC(myId)
                     }
-                } else{
-                    Toast.makeText(this, "適切でないファイル形式です", Toast.LENGTH_SHORT).show()
                 }
             }
         } catch (t: UnsupportedEncodingException) {
             Toast.makeText(this, "not supported", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun getFile(uri: Uri): File? {
+        val path: String? = getPath(uri)
+        if (path != null) {
+            file = File(path)
+            return file
+        }
+        val mime = contentResolver.getType(uri)
+        val ext = MimeTypeMap.getSingleton().getExtensionFromMimeType(mime)
+        val filename = "user_icon_id_" + myId.toString() + "." + ext
+
+        file = File(cacheDir, filename)
+        val fos = FileOutputStream(file)
+        fos.write(IOUtils.toByteArray(contentResolver.openInputStream(uri)))
+        fos.close()
+        return file
+    }
+
+    fun getPath(uri: Uri): String? {
+        val context = this
+
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            if ("com.android.externalstorage.documents" == uri.getAuthority()) {// ExternalStorageProvider
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                val type = split[0]
+                return if ("primary".equals(type, ignoreCase = true)) {
+                    Environment.getExternalStorageDirectory().toString() + "/" + split[1]
+                } else {
+                    "/stroage/" + type + "/" + split[1]
+                }
+            } else if ("com.android.providers.downloads.documents" == uri.getAuthority()) {// DownloadsProvider
+                val id = DocumentsContract.getDocumentId(uri)
+                val contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(id))
+                return getDataColumn(context, contentUri, null, null)
+            } else if ("com.android.providers.media.documents" == uri.getAuthority()) {// MediaProvider
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                val type = split[0]
+                var contentUri: Uri? = null
+                contentUri = MediaStore.Files.getContentUri("external")
+                val selection = "_id=?"
+                val selectionArgs = arrayOf(split[1])
+                return getDataColumn(context, contentUri, selection, selectionArgs)
+            }
+        }
+        if ("content".equals(uri.getScheme(), ignoreCase = true)) {//MediaStore
+            return getDataColumn(context, uri, null, null)
+        } else if ("file".equals(uri.getScheme(), ignoreCase = true)) {// File
+            return uri.getPath()
+        }
+        return null
+    }
+
+    fun getDataColumn(context: Context?, uri: Uri, selection: String?,
+                      selectionArgs: Array<String>?): String? {
+        var cursor: Cursor? = null
+        val projection = arrayOf(MediaStore.Files.FileColumns.DATA)
+        try {
+            cursor = context!!.contentResolver.query(
+                    uri, projection, selection, selectionArgs, null)
+            if (cursor != null && cursor!!.moveToFirst()) {
+                val cindex = cursor!!.getColumnIndexOrThrow(projection[0])
+                return cursor.getString(cindex)
+            }
+        } catch (e: Exception) {
+            // no op
+        } finally {
+            cursor?.close()
+        }
+        return null
     }
 
     //ユーザー情報をセットする
